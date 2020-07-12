7e367901cb6ddfcb1ba639f638447e1d5476e980
rodrigoasbun <gdrodrigo8@gmail.com>
2020-07-11 23:13:27 -0700

    Employee schema

--- models/Employee.js
+++ models/Employee.js
@@ -1,7 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const EmployeeSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  middleName: { type: String },
+  employeeID: { type: Number, required: true },
+  password: { type: String, required: true },
+  dateOfHire: { type: Date, required: true },
+  email: { type: String, required: true },
+  payRate: { type: Date, required: true },
+  carNumber: { type: String },
+  assignments: [],
+});
 
-})
-
-module.exports = Employee = mongoose.model('employee', EmployeeSchema)
+module.exports = Employee = mongoose.model("employee", EmployeeSchema);
